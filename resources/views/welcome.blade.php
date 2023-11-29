0b61672c96a83621705474d34b2009c2aee3d826
malwih <malwihidayattogatorop@gmail.com>
2023-11-29 20:22:30 +0700

    Menambahkan navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     
-<!-- Navbar -->
+<!-- Menu Navbar -->
 <nav class="bg-white border-gray-200 dark:bg-gray-900">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
   <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
